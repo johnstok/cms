180a6e794caec8e354cf6a29a7775362238b0f89
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-06-20 13:40:18 +0000

    Added core-domain module.

---
+++ core-domain/src/main/java/ccc/domain/App.java
@@ -0,0 +1,13 @@
+package ccc.domain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
