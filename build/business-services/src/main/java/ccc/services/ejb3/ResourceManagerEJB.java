5db0b16df1f7177f25bc5c4fccbaf1ce03280073
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-06-25 16:13:27 +0000

    Moved to using JPA for persistence.

--- build/business-services/src/main/java/ccc/services/ejb3/ResourceManagerEJB.java
+++ build/business-services/src/main/java/ccc/services/ejb3/ResourceManagerEJB.java
@@ -15,13 +15,8 @@ import static javax.ejb.TransactionAttributeType.REQUIRED;
 
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import ccc.domain.Resource;
 import ccc.services.ResourceManager;
@@ -36,36 +31,14 @@ import ccc.services.ResourceManager;
 @TransactionAttribute(REQUIRED)
 public class ResourceManagerEJB implements ResourceManager {
    
-   private static final SessionFactory sessionFactory;
-   
-   static {
-      try {
-          // Create the SessionFactory from hibernate.cfg.xml
-          sessionFactory = new Configuration().configure().buildSessionFactory();
-      } catch (Throwable ex) {
-          // Make sure you log the exception, as it might be swallowed
-          System.err.println("Initial SessionFactory creation failed." + ex);
-          throw new ExceptionInInitializerError(ex);
-      }
-  }
-
+   @PersistenceContext(unitName="ccc-persistence") private EntityManager em;
    /**
     * @see ccc.services.ResourceManager#create()
     */
    @Override
    public void create() {
-      Session s;
-      try {
-         s = ((SessionFactory) new InitialContext().lookup("ccc-persistence")).getCurrentSession();
-      } catch (HibernateException e) {
-         // TODO Auto-generated catch block
-         throw new RuntimeException(e);
-      } catch (NamingException e) {
-         // TODO Auto-generated catch block
-         throw new RuntimeException(e);
-      }
       Resource r = new Resource();
-      s.save(r);
+      em.persist(r);
       System.out.println("created");      
    }
 
