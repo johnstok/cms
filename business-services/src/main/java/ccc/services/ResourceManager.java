01357026b013838411556c8454b14973ccdab66e
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-06-25 12:26:28 +0000

    Added a module for EJBs.

---
+++ business-services/src/main/java/ccc/services/ResourceManager.java
@@ -0,0 +1,23 @@
+/*-----------------------------------------------------------------------------
+ * Copyright (c) 2008 Civic Computing Ltd
+ * All rights reserved.
+ *
+ * Revision      $Rev$
+ * Modified by   $Author$
+ * Modified on   $Date$
+ *
+ * Changes: see SubVersion log
+ *-----------------------------------------------------------------------------
+ */
+package ccc.services;
+
+
+/**
+ * TODO Add Description for this type.
+ *
+ * @author Civic Computing Ltd
+ */
+public interface ResourceManager {
+
+   public void create();
+}
