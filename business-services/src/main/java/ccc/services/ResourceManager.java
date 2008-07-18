ace347a9e4c0343648c633e5001ff5ddf90f998a
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-07-18 14:15:19 +0000

    #98: Added createRoot() and createFolder() methods to the ResourceManager.

--- business-services/src/main/java/ccc/services/ResourceManager.java
+++ business-services/src/main/java/ccc/services/ResourceManager.java
@@ -9,6 +9,7 @@
  * Changes: see SubVersion log
  *-----------------------------------------------------------------------------
  */
+
 package ccc.services;
 
 import ccc.domain.Resource;
@@ -22,11 +23,23 @@ import ccc.domain.ResourcePath;
  */
 public interface ResourceManager {
 
-   /**
-    * Lookup a resource, given its absolute path.
-    *
-    * @param path The absolute path to the resource.
-    * @return The resource.
-    */
-   Resource lookup(ResourcePath path);
+    /**
+     * Lookup a resource, given its absolute path.
+     *
+     * @param path The absolute path to the resource.
+     * @return The resource.
+     */
+    Resource lookup(ResourcePath path);
+
+    /**
+     * Create a folder, based on the specified path.
+     *
+     * @param pathString
+     */
+    void createFolder(String pathString);
+
+    /**
+     * Create the root folder for content.
+     */
+    void createRoot();
 }
--- services-ejb3/src/main/java/ccc/services/ejb3/ResourceManagerEJB.java
+++ services-ejb3/src/main/java/ccc/services/ejb3/ResourceManagerEJB.java
@@ -9,19 +9,23 @@
  * Changes: see SubVersion log
  *-----------------------------------------------------------------------------
  */
+
 package ccc.services.ejb3;
 
-import static ccc.domain.Queries.RESOURCE_BY_URL;
-import static javax.ejb.TransactionAttributeType.REQUIRED;
+import static ccc.domain.Queries.*;
+import static javax.ejb.TransactionAttributeType.*;
 
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
+import ccc.domain.CCCException;
 import ccc.domain.Folder;
 import ccc.domain.PredefinedResourceNames;
 import ccc.domain.Resource;
+import ccc.domain.ResourceName;
 import ccc.domain.ResourcePath;
 import ccc.services.ResourceManager;
 
@@ -35,32 +39,69 @@ import ccc.services.ResourceManager;
 @TransactionAttribute(REQUIRED)
 public class ResourceManagerEJB implements ResourceManager {
 
-   @PersistenceContext(unitName="ccc-persistence")
-       private final EntityManager em;
+    @PersistenceContext(unitName = "ccc-persistence")
+    private final EntityManager em;
 
-   /**
-    * Constructor.
-    *
-    * @param entityManager A JPA entity manager.
-    */
-   public ResourceManagerEJB(final EntityManager entityManager) {
-      em = entityManager;
-   }
+    /**
+     * Constructor.
+     *
+     * @param entityManager A JPA entity manager.
+     */
+    public ResourceManagerEJB(final EntityManager entityManager) {
 
-   /**
-    * @see ResourceManager#lookup(java.lang.String)
-    */
-   @Override
-   public Resource lookup(final ResourcePath path) {
+        em = entityManager;
+    }
 
-       Object singleResult =
-           em.createNamedQuery(RESOURCE_BY_URL)
-               .setParameter("url", PredefinedResourceNames.CONTENT)
-               .getSingleResult();
+    /**
+     * @see ResourceManager#lookup(java.lang.String)
+     */
+    @Override
+    public Resource lookup(final ResourcePath path) {
+        return contentRoot().navigateTo(path);
+    }
 
-       Folder contentRoot = Folder.class.cast(singleResult);
+    /**
+     * @see ccc.services.ResourceManager#createFolder(java.lang.String)
+     */
+    @Override
+    public void createFolder(final String pathString) {
+        ResourcePath path = new ResourcePath(pathString);
+        Folder currentFolder = contentRoot();
+        for (ResourceName name : path.elements()) {
+            try {
+                currentFolder = currentFolder.findEntryByName(name).asFolder();
+            } catch(CCCException e) {
+                Folder newFolder = new Folder(name);
+                currentFolder.add(newFolder);
+                currentFolder = newFolder;
+            }
+        }
+    }
 
-       return contentRoot.navigateTo(path);
-   }
+    /**
+     * @see ccc.services.ResourceManager#createRoot()
+     */
+    @Override
+    public void createRoot() {
+        Folder contentRoot = contentRoot();
+        if (null == contentRoot) {
+            em.persist(new Folder(PredefinedResourceNames.CONTENT));
+        }
+    }
 
+    /**
+     * Look up the root folder for the content hierarchy.
+     * TODO: Factor this method to a helper class?
+     *
+     * @return
+     */
+    private Folder contentRoot() {
+
+        Query q = em.createNamedQuery(RESOURCE_BY_URL);
+        q.setParameter("url", PredefinedResourceNames.CONTENT);
+        Object singleResult = q.getSingleResult();
+
+        Folder contentRoot = Folder.class.cast(singleResult);
+        return contentRoot;
+    }
 }
