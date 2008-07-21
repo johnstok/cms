9a9cae21f9ae9db331ec8fe9d71cd94bf687dc34
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-07-21 13:31:16 +0000

    Added some build scripts for windows.

---
+++ build/redeploy.cmd
@@ -0,0 +1,17 @@
+@ echo off
+cls
+call mvn clean install
+pause
+cls
+echo  --------------------
+echo   CCC7 Deploy Script 
+echo  --------------------
+cd core-domain
+call mvn jboss:deploy
+cd ..
+cd business-services
+call mvn jboss:deploy
+cd ..
+cd services-ejb3
+call mvn jboss:deploy
+cd ..
