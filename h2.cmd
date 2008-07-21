5319c0da44b4d96e8cf54f29e3f56cbd65538045
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-07-21 13:31:16 +0000

    Added some build scripts for windows.

---
+++ h2.cmd
@@ -0,0 +1,10 @@
+@ echo off
+cls
+set h2_version=1.0.74
+echo  ----------------
+echo   H2 [V. %h2_version%]
+echo  ----------------
+%homedrive%
+cd \
+cd %homepath%\.m2\repository\com\h2database\h2\%h2_version%
+java -cp h2-%h2_version%.jar org.h2.tools.Server -tcpAllowOthers
