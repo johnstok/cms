d22f257646d3d3dfcc7caec1fd904cf001c3d790
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-07-23 17:40:58 +0000

    * Fixed up a number of style issues in content-server.  * Added an error handling servlet.

---
+++ content-server/src/main/java/ccc/content/server/SessionKeys.java
@@ -0,0 +1,27 @@
+/*-----------------------------------------------------------------------------
+ * Copyright (c) 2008 Civic Computing Ltd
+ * All rights reserved.
+ *
+ * Revision      $Rev$
+ * Modified by   $Author$
+ * Modified on   $Date$
+ *
+ * Changes: see subversion log
+ *-----------------------------------------------------------------------------
+ */
+package ccc.content.server;
+
+
+/**
+ * Constants for use with CCC Servlets.
+ *
+ * @author Civic Computing Ltd
+ */
+final class SessionKeys {
+
+    private SessionKeys() { super(); }
+
+    /** EXCEPTION_KEY : String. */
+    static final String EXCEPTION_KEY = "ccc.exception";
+
+}
