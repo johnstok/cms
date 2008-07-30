e7f14db74ae3f0626ad28a810101eef805a25afc
Petteri <petteri@civicuk.com>
2008-07-30 09:08:11 +0000

    #101: Added workarounds for horizontal split panel height and tree item offset problems.

--- build/content-creator/src/main/java/ccc/view/contentcreator/client/ContentCreator.java
+++ build/content-creator/src/main/java/ccc/view/contentcreator/client/ContentCreator.java
@@ -7,6 +7,9 @@ import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
 import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.WindowResizeListener;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HorizontalSplitPanel;
 import com.google.gwt.user.client.ui.Label;
@@ -49,20 +52,32 @@ public class ContentCreator implements EntryPoint {
         final Label label = new Label("Hello");
 
         final HorizontalSplitPanel hsp = new HorizontalSplitPanel();
-        hsp.setSplitPosition("50%");
+        hsp.setSplitPosition("35%");
         hsp.setLeftWidget(t);
         hsp.setRightWidget(label);
 
-        RootPanel.get().setSize("800px", "800px");
+        // TODO 30 Jul 2008 petteri: Check if Window.getClientHeight() can be 
+        // replaced with css markup
+
+        RootPanel.get().setSize("100%", 
+            Window.getClientHeight()+"px");
+
+        Window.addWindowResizeListener(new WindowResizeListener() {
+
+            public void onWindowResized(int width, int height) {
+                RootPanel.get().setSize("100%", 
+                    height+"px");
+            }
+        });        
         RootPanel.get().add(hsp);
     }
 
     /**
      * TODO: Add a description of this method.
      *
-     * @param root
+     * @param parentItem
      */
-    private void populate(final TreeItem root) {
+    private void populate(final TreeItem parentItem) {
 
         final ResourceServiceAsync resourceService = (ResourceServiceAsync) GWT.create(ResourceService.class);
 
@@ -70,7 +85,13 @@ public class ContentCreator implements EntryPoint {
 
             public void onSuccess(String result) {
 
-                root.removeItems();
+                parentItem.removeItems();
+                /*
+                 * Bug in GWT 1.5 RC 1
+                 * http://code.google.com/p/google-web-toolkit/issues/detail?id=2491
+                 */ 
+                DOM.setStyleAttribute(parentItem.getElement(),"paddingLeft","0px");
+
                 JSONValue jsonResult = JSONParser.parse(result);
                 JSONArray entries = jsonResult.isObject().get("entries").isArray();
                 for (int i=0; i<entries.size(); i++) {
@@ -80,7 +101,7 @@ public class ContentCreator implements EntryPoint {
                     if(entry.get("type").isString().stringValue().equals("FOLDER")) {
                         item.addItem("Loading contents...");
                     }
-                    root.addItem(item);
+                    parentItem.addItem(item);
                 }
             }
 
@@ -90,7 +111,7 @@ public class ContentCreator implements EntryPoint {
             }
         };
 
-        final String absolutePath = GWTSupport.calculatePathForTreeItem(root);
+        final String absolutePath = GWTSupport.calculatePathForTreeItem(parentItem);
         resourceService.getResource(absolutePath, callback);
 
     }
