89dfe8a6914d453df7fa0bcc37d2d2589ee146d3
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-07-31 08:48:57 +0000

    #72: Added a JsonCallback class.

---
+++ content-creator/src/main/java/ccc/view/contentcreator/client/JSONCallback.java
@@ -0,0 +1,48 @@
+/*-----------------------------------------------------------------------------
+ * Copyright (c) 2008 Civic Computing Ltd
+ * All rights reserved.
+ *
+ * Revision      $Rev$
+ * Modified by   $Author$
+ * Modified on   $Date$
+ *
+ * Changes: see subversion log
+ *-----------------------------------------------------------------------------
+ */
+package ccc.view.contentcreator.client;
+
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+
+/**
+ * TODO Add Description for this type.
+ *
+ * @author Civic Computing Ltd
+ */
+public abstract class JSONCallback implements AsyncCallback<String> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final void onFailure(final Throwable caught) {
+        Window.alert("Error: "+caught.getMessage());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final void onSuccess(final String result) {
+        final JSONValue jsonResult = JSONParser.parse(result);
+        onSuccess(jsonResult);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public abstract void onSuccess(JSONValue result);
+}
