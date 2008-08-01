79b49ad455ca6fbd9109117de85b99f309f88cb8
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-08-01 15:04:04 +0000

    #33: Added support for Finnish in content-creator.

---
+++ build/content-creator/src/main/java/ccc/view/contentcreator/client/Constants.java
@@ -0,0 +1,76 @@
+/*-----------------------------------------------------------------------------
+ * Copyright (c) 2008 Civic Computing Ltd
+ * All rights reserved.
+ *
+ * Revision      $Rev$
+ * Modified by   $Author$
+ * Modified on   $Date$
+ *
+ * Changes: see subversion log
+ *-----------------------------------------------------------------------------
+ */
+package ccc.view.contentcreator.client;
+
+/**
+ * Constants for i18n.
+ *
+ * @author Civic Computing Ltd
+ */
+public interface Constants extends com.google.gwt.i18n.client.Constants {
+
+    /**
+     * "Update Content".
+     *
+     * @return The constant, in the appropriate locale.
+     */
+    @DefaultStringValue("Update Content")
+    String updateContent();
+
+    /**
+     * "Cancel".
+     *
+     * @return The constant, in the appropriate locale.
+     */
+    @DefaultStringValue("Cancel")
+    String cancel();
+
+    /**
+     * "Save".
+     *
+     * @return The constant, in the appropriate locale.
+     */
+    @DefaultStringValue("Save")
+    String save();
+
+    /**
+     * "Edit".
+     *
+     * @return The constant, in the appropriate locale.
+     */
+    @DefaultStringValue("Edit")
+    String edit();
+
+    /**
+     * "Type".
+     *
+     * @return The constant, in the appropriate locale.
+     */
+    @DefaultStringValue("Type")
+    String type();
+
+    /**
+     * "Title".
+     *
+     * @return The constant, in the appropriate locale.
+     */
+    @DefaultStringValue("Title")
+    String title();
+
+    /**
+     * "Actions".
+     *
+     * @return The constant, in the appropriate locale.
+     */
+    @DefaultStringValue("Actions")
+    String actions();
+}
