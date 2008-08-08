eee3d894064eec7e26e8710b390c2ef35050de59
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-08-08 09:32:45 +0000

    Updated h2 startup script.

--- build/h2.cmd
+++ build/h2.cmd
@@ -4,7 +4,7 @@ set h2_version=1.0.74
 echo  ----------------
 echo   H2 [V. %h2_version%]
 echo  ----------------
-%homedrive%
+%SystemDrive%
 cd \
-cd %homepath%\.m2\repository\com\h2database\h2\%h2_version%
+cd %USERPROFILE%\.m2\repository\com\h2database\h2\%h2_version%
 java -cp h2-%h2_version%.jar org.h2.tools.Server -tcpAllowOthers
