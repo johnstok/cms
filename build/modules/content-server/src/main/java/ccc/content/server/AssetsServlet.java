54c04f02853af4a0b40862b73bacebac3625d709
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-09-03 10:12:04 +0000

    #143: Added basic serving of assets as files.

---
+++ build/modules/content-server/src/main/java/ccc/content/server/AssetsServlet.java
@@ -0,0 +1,71 @@
+/*-----------------------------------------------------------------------------
+ * Copyright (c) 2008 Civic Computing Ltd.
+ * All rights reserved.
+ *
+ * Revision      $Rev$
+ * Modified by   $Author$
+ * Modified on   $Date$
+ *
+ * Changes: see subversion log.
+ *-----------------------------------------------------------------------------
+ */
+
+package ccc.content.server;
+
+import java.io.IOException;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ccc.commons.JNDI;
+import ccc.commons.Registry;
+import ccc.domain.File;
+import ccc.domain.ResourcePath;
+import ccc.services.AssetManager;
+import ccc.services.DataManager;
+
+
+/**
+ * The AssetsServlet class serves resources from the 'assets' root. It can only
+ * serve {@link File} resources. For a file resource it retrieves the
+ * corresponding {@link Data} instance and writes the raw data as the body
+ * of the HTTP response.
+ *
+ * @author Civic Computing Ltd.
+ */
+public class AssetsServlet extends HttpServlet {
+
+    /** _registry : Registry. */
+    private Registry _registry = new JNDI();
+
+    /**
+     * Constructor.
+     */
+    public AssetsServlet() { /* NO-OP */ }
+
+    /**
+     * Constructor.
+     *
+     * @param registry
+     */
+    public AssetsServlet(final Registry registry) {
+        _registry = registry;
+    }
+
+    /** {@inheritDoc}
+     * @throws IOException */
+    @Override protected void doGet(final HttpServletRequest req,
+                                   final HttpServletResponse resp)
+                            throws IOException {
+        final ResourcePath path = new ResourcePath(req.getPathInfo());
+        final AssetManager am = _registry.get("AssetManagerEJB/local");
+        final File f = am.lookup(path).as(File.class);
+        final ServletOutputStream os = resp.getOutputStream();
+        final DataManager dm = _registry.get("DataManagerEJB/local");
+        dm.retrieve(f.fileData(), os);
+    }
+
+
+}
