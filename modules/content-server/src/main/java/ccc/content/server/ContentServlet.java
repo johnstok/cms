0dc8216277d8eb2163a5ea2cacde7fded54a3d12
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-09-05 09:50:19 +0000

    #50 Refactored ContentServlet to use VelocityProcessor.

--- modules/content-server/src/main/java/ccc/content/server/ContentServlet.java
+++ modules/content-server/src/main/java/ccc/content/server/ContentServlet.java
@@ -13,23 +13,15 @@
 package ccc.content.server;
 
 import java.io.IOException;
-import java.io.StringWriter;
 import java.nio.charset.Charset;
-import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.VelocityEngine;
-import org.apache.velocity.exception.MethodInvocationException;
-import org.apache.velocity.exception.ParseErrorException;
-import org.apache.velocity.exception.ResourceNotFoundException;
-import org.apache.velocity.runtime.RuntimeConstants;
-
 import ccc.commons.Registry;
 import ccc.commons.Resources;
+import ccc.commons.VelocityProcessor;
 import ccc.domain.CCCException;
 import ccc.domain.Folder;
 import ccc.domain.Page;
@@ -237,44 +229,12 @@ public final class ContentServlet extends CCCServlet {
 
     /**
      * Render a resource with the specified template.
-     * TODO: should use VelocityProcessor.
      *
      * @param resource The resource that will be rendered.
      * @param template The template used to render the resource.
      * @return The html rendering as a string.
      */
     public String render(final Resource resource, final String template) {
-
-        final StringWriter html = new StringWriter();
-
-        final Properties velocityProperties = new Properties();
-        velocityProperties.setProperty(
-            RuntimeConstants.RUNTIME_LOG_LOGSYSTEM_CLASS,
-            "org.apache.velocity.runtime.log.Log4JLogChute");
-        velocityProperties.setProperty(
-            "runtime.log.logsystem.log4j.logger",
-            getClass().getName());
-
-        try {
-            final VelocityEngine ve = new VelocityEngine(velocityProperties);
-            ve.init();
-            final VelocityContext context = new VelocityContext();
-            context.put("resource", resource);
-
-            ve.evaluate(context, html, "????", template);
-
-        } catch (final ParseErrorException e) {
-            throw new CCCException(e);
-        } catch (final MethodInvocationException e) {
-            throw new CCCException(e);
-        } catch (final ResourceNotFoundException e) {
-            throw new CCCException(e);
-        } catch (final IOException e) {
-            throw new CCCException(e);
-        } catch (final Exception e) {
-            throw new CCCException(e);
-        }
-
-        return html.toString();
+        return new VelocityProcessor().render(resource, template);
     }
 }
