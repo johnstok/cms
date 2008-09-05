97a61ad96d9eb23169b9fe5cafafcda763b80149
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-09-05 09:38:43 +0000

    #50: Added macro support for VelocityProcessor.

---
+++ modules/core-domain/src/test/java/ccc/commons/VelocityProcessorTest.java
@@ -0,0 +1,42 @@
+/*-----------------------------------------------------------------------------
+ * Copyright (c) 2008 Civic Computing Ltd.
+ * All rights reserved.
+ *
+ * Revision      $Rev$
+ * Modified by   $Author$
+ * Modified on   $Date$
+ *
+ * Changes: see subversion log.
+ *-----------------------------------------------------------------------------
+ */
+package ccc.commons;
+
+import junit.framework.TestCase;
+import ccc.domain.Page;
+import ccc.domain.ResourceName;
+
+
+/**
+ * TODO: Add Description for this type.
+ *
+ * @author Civic Computing Ltd.
+ */
+public class VelocityProcessorTest extends TestCase {
+
+    /**
+     * Test.
+     */
+    public void testMacroLoading() {
+
+        // ARRANGE
+        final VelocityProcessor vp = new VelocityProcessor();
+
+        // ACT
+        final String actual =
+            vp.render(new Page(new ResourceName("foo")), "#hello()");
+
+
+        // ASSERT
+        assertEquals("hello!", actual);
+    }
+}
