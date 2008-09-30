5fd7b5b3aadfad2126baa13275f8718fc101b4b9
Petteri <petteri@civicuk.com>
2008-09-30 11:27:34 +0000

    #172: Missed a new file.

---
+++ build/modules/core-domain/src/main/java/ccc/domain/CreatorRoles.java
@@ -0,0 +1,24 @@
+/*-----------------------------------------------------------------------------
+ * Copyright (c) 2008 Civic Computing Ltd.
+ * All rights reserved.
+ *
+ * Revision      $Rev$
+ * Modified by   $Author$
+ * Modified on   $Date$
+ *
+ * Changes: see subversion log.
+ *-----------------------------------------------------------------------------
+ */
+package ccc.domain;
+
+
+/**
+ * TODO: Add Description for this type.
+ *
+ * @author Civic Computing Ltd.
+ */
+public enum CreatorRoles {
+    CONTENT_CREATOR,
+    SITE_BUILDER,
+    ADMINISTRATOR;
+}
