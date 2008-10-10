a7cd4447105b2f2d72d842b68247f68686c132fa
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-10-10 15:07:06 +0000

    #178: Updated to selenium snapshot - right click now supported.

--- build/selenium-up.cmd
+++ build/selenium-up.cmd
@@ -1,4 +1,11 @@
-@echo off
-cd modules\content-server
-call mvn selenium:start-server -Dport=5555
+rem @ echo off
+cls
+set selenium_version=1.0-SNAPSHOT
+echo  ---------------------------
+echo   Selenium Server [V. %selenium_version%]
+echo  ---------------------------
+%SystemDrive%
+cd \
+cd %USERPROFILE%\.m2\repository\org\openqa\selenium\server\selenium-server\%selenium_version%
+java -jar selenium-server-%selenium_version%-standalone.jar -port 5555
 exit
