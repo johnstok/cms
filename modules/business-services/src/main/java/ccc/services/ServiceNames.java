ec693f73d9e1dd179f6ac41b734c335d042b5abf
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-10-23 12:43:16 +0000

    Abstracted JNDI bindings for business services into ServiceNames class.

---
+++ modules/business-services/src/main/java/ccc/services/ServiceNames.java
@@ -0,0 +1,49 @@
+/*-----------------------------------------------------------------------------
+ * Copyright (c) 2008 Civic Computing Ltd.
+ * All rights reserved.
+ *
+ * Revision      $Rev$
+ * Modified by   $Author$
+ * Modified on   $Date$
+ *
+ * Changes: see subversion log.
+ *-----------------------------------------------------------------------------
+ */
+package ccc.services;
+
+
+/**
+ * TODO: Add Description for this type.
+ *
+ * @author Civic Computing Ltd.
+ */
+public final class ServiceNames {
+
+
+    /*
+     * LOCAL
+     */
+    public static final String CONTENT_MANAGER_LOCAL =
+        "application-ear-7.0.0-SNAPSHOT/ContentManager/local";
+
+    public static final String ASSET_MANAGER_LOCAL =
+        "application-ear-7.0.0-SNAPSHOT/AssetManager/local";
+
+    public static final String DATA_MANAGER_LOCAL =
+        "application-ear-7.0.0-SNAPSHOT/DataManager/local";
+
+    public static final String USER_MANAGER_LOCAL =
+        "CCC7/UserManager/local";
+
+    /*
+     * REMOTE
+     */
+    public static final String CONTENT_MANAGER_REMOTE =
+        "application-ear-7.0.0-SNAPSHOT/ContentManager/remote";
+
+    public static final String ASSET_MANAGER_REMOTE =
+        "application-ear-7.0.0-SNAPSHOT/AssetManager/remote";
+
+    public static final String USER_MANAGER_REMOTE =
+        "application-ear-7.0.0-SNAPSHOT/UserManager/remote";
+}
