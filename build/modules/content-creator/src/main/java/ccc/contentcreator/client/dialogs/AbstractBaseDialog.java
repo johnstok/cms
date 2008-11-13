dce32fec2cee205d90b7098922b7cb14df462e41
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-11-13 16:14:08 +0000

    #74: Updated column model and date rendering.

--- build/modules/content-creator/src/main/java/ccc/contentcreator/client/dialogs/AbstractBaseDialog.java
+++ build/modules/content-creator/src/main/java/ccc/contentcreator/client/dialogs/AbstractBaseDialog.java
@@ -30,6 +30,7 @@ public abstract class AbstractBaseDialog
     /** _constants : UIConstants. */
     protected final UIConstants _constants = Globals.uiConstants();
 
+
     /**
      * Constructor.
      *
@@ -41,4 +42,18 @@ public abstract class AbstractBaseDialog
         setWidth(Globals.DEFAULT_WIDTH);
         setHeight(Globals.DEFAULT_HEIGHT);
     }
+
+
+    /** CONTEXT_MENU_WIDTH : int. */
+    public static final int CONTEXT_MENU_WIDTH = 130;
+    /** PERCENT_10 : float. */
+    public static final float PERCENT_10 = .1f;
+    /** PERCENT_30 : float. */
+    public static final float PERCENT_30 = .3f;
+    /** PERCENT_40 : float. */
+    public static final float PERCENT_40 = .4f;
+    /** PERCENT_50 : float. */
+    public static final float PERCENT_50 = .5f;
+    /** PERCENT_70 : float. */
+    public static final float PERCENT_70 = .7f;
 }
