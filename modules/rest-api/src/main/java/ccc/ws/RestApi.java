8c0b037d353fdb16cfa19226a1034d0744a271e4
Keith Webster Johnston <k.w.johnston@gmail.com>
2008-12-11 14:06:25 +0000

    Spike: investigated generating HTML as output from rest-api.

---
+++ modules/rest-api/src/main/java/ccc/ws/RestApi.java
@@ -0,0 +1,50 @@
+/*-----------------------------------------------------------------------------
+ * Copyright (c) 2008 Civic Computing Ltd.
+ * All rights reserved.
+ *
+ * Revision      $Rev$
+ * Modified by   $Author$
+ * Modified on   $Date$
+ *
+ * Changes: see subversion log.
+ *-----------------------------------------------------------------------------
+ */
+package ccc.ws;
+
+import java.util.Collection;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.jboss.resteasy.annotations.cache.NoCache;
+
+import ccc.services.api.Queries;
+import ccc.services.api.ResourceSummary;
+
+
+/**
+ * This class exposes parts of our public API using JAX-RS.
+ *
+ * @author Civic Computing Ltd.
+ */
+@Path("")
+@Produces("text/html")
+public class RestApi {
+
+    private Queries _queries;
+
+    /**
+     * Constructor.
+     *
+     * @param queries The {@link Queries} implementation to use.
+     */
+    public RestApi(final Queries queries) {
+        _queries = queries;
+    }
+
+    @GET @Path("/roots") @NoCache
+    public Collection<ResourceSummary> roots() {
+        return _queries.roots();
+    }
+}
---
+++ modules/rest-api/src/test/java/ccc/ws/RestApiTest.java
@@ -0,0 +1,69 @@
+/*-----------------------------------------------------------------------------
+ * Copyright (c) 2008 Civic Computing Ltd.
+ * All rights reserved.
+ *
+ * Revision      $Rev$
+ * Modified by   $Author$
+ * Modified on   $Date$
+ *
+ * Changes: see subversion log.
+ *-----------------------------------------------------------------------------
+ */
+package ccc.ws;
+
+import static org.easymock.EasyMock.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import junit.framework.TestCase;
+import ccc.services.api.Queries;
+import ccc.services.api.ResourceSummary;
+
+
+/**
+ * TODO: Add Description for this type.
+ *
+ * @author Civic Computing Ltd.
+ */
+public class RestApiTest
+    extends
+        TestCase {
+
+    /**
+     * Test.
+     */
+    public void testRootsMethodCallsQueriesService() {
+
+        // ARRANGE
+        expect(_queries.roots()).andReturn(_rs);
+        replay(_queries);
+
+        // ACT
+        final Collection<ResourceSummary> actual = _unit.roots();
+
+        // ASSERT
+        assertSame(_rs, actual);
+        verify(_queries);
+    }
+
+
+
+    /** {@inheritDoc} */
+    @Override
+    protected void setUp() throws Exception {
+        _queries = createStrictMock(Queries.class);
+        _unit = new RestApi(_queries);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void tearDown() throws Exception {
+        _queries = null;
+        _unit = null;
+    }
+
+    private Queries _queries;
+    private RestApi _unit;
+    private Collection<ResourceSummary> _rs = new ArrayList<ResourceSummary>();
+}
