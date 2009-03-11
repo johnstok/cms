acd7d903dc63f2f580759e3b30309b791de1c440
Keith Webster Johnston <k.w.johnston@gmail.com>
2009-03-11 11:55:09 +0000

    Refactoring EJBs.

--- modules/services-ejb3/src/main/java/ccc/services/ejb3/local/SchedulerEJB.java
+++ modules/services-ejb3/src/main/java/ccc/services/ejb3/local/SchedulerEJB.java
@@ -30,6 +30,7 @@ import javax.ejb.TransactionAttribute;
 import org.apache.log4j.Logger;
 
 import ccc.actions.Action;
+import ccc.services.ActionExecutor;
 import ccc.services.Scheduler;
 import ccc.services.ejb3.support.Dao;
 
