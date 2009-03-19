eeb268fb6a8e2981e5ae1b4104048c8dbc972467
Keith Webster Johnston <k.w.johnston@gmail.com>
2009-03-19 15:18:01 +0000

    #52: Enhanced security to redirect to a login page for resources where the current user has insufficient privileges.

--- build/modules/rendering-engine/src/main/java/ccc/content/server/DefaultLocator.java
+++ build/modules/rendering-engine/src/main/java/ccc/content/server/DefaultLocator.java
@@ -16,6 +16,7 @@ import java.util.UUID;
 import ccc.commons.DBC;
 import ccc.domain.Resource;
 import ccc.domain.ResourcePath;
+import ccc.domain.User;
 import ccc.services.StatefulReader;
 
 
@@ -50,7 +51,7 @@ public class DefaultLocator
     /** {@inheritDoc} */
     @Override
     public Resource locate(final ResourcePath resourcePath) {
-        return _reader.lookup(_rootName, resourcePath);
+        return applySecurity(_reader.lookup(_rootName, resourcePath));
     }
 
 
@@ -58,5 +59,20 @@ public class DefaultLocator
     @Override
     public Resource locate(final UUID resourceId) {
         throw new NotFoundException();
+    }
+
+
+    private Resource applySecurity(final Resource r) {
+        if (null==r) {
+            return null;
+        }
+        User u = _reader.loggedInUser();
+        if (null==u) {
+            u = new User("anonymous");
+        }
+        if (r.isAccessibleTo(u)) {
+            return r;
+        }
+        throw new AuthenticationRequiredException(r);
     }
 }
