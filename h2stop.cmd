11953bd82c8293898ac78ea3ee7515742c1090ea
Petteri <petteri@civicuk.com>
2009-04-03 09:18:17 +0000

    Modified H2 start and stop scripts. Does not stat web or pg services.

---
+++ h2stop.cmd
@@ -0,0 +1,10 @@
+@ echo off
+cls
+set h2_version=1.1.103
+echo  -----------------
+echo   H2 [V. %h2_version%]
+echo  -----------------
+%SystemDrive%
+cd \
+cd %USERPROFILE%\.m2\repository\com\h2database\h2\%h2_version%
+java -cp h2-%h2_version%.jar org.h2.tools.Server -tcpShutdown  tcp://localhost:9092 
