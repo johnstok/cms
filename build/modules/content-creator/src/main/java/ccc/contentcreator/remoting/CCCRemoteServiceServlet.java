3c9a5b7db57449474a813acb6c9520633d06ee58
Petteri <petteri@civicuk.com>
2009-04-22 15:23:19 +0000

    Serialization policy file loading.

--- build/modules/content-creator/src/main/java/ccc/contentcreator/remoting/CCCRemoteServiceServlet.java
+++ build/modules/content-creator/src/main/java/ccc/contentcreator/remoting/CCCRemoteServiceServlet.java
@@ -11,10 +11,20 @@
  */
 package ccc.contentcreator.remoting;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.ParseException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import ccc.commons.CCCProperties;
 import ccc.services.ServiceLookup;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.google.gwt.user.server.rpc.SerializationPolicy;
+import com.google.gwt.user.server.rpc.SerializationPolicyLoader;
 
 
 /**
@@ -22,22 +32,66 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
  *
  * @author Civic Computing Ltd.
  */
-public abstract class CCCRemoteServiceServlet
-    extends
-        RemoteServiceServlet {
+public abstract class CCCRemoteServiceServlet extends RemoteServiceServlet {
 
     protected ServiceLookup _services =
         new ServiceLookup(CCCProperties.get("application.name"));
 
-//    /** {@inheritDoc} */
-//    @Override
-//    protected SerializationPolicy doGetSerializationPolicy(final HttpServletRequest arg0,
-//                                                           final String arg1,
-//                                                           final String arg2) {
-//        String context = arg0.getContextPath();
-//        if (context.startsWith("/")) {
-//            context = context.substring(1);
-//        }
-//        return super.doGetSerializationPolicy(arg0, arg1+context, arg2);
-//    }
+    /** {@inheritDoc} */
+    @Override
+    protected SerializationPolicy doGetSerializationPolicy(final HttpServletRequest request,
+                                                           final String moduleBaseURL,
+                                                           final String strongName) {
+
+        SerializationPolicy serializationPolicy = null;
+        final String serializationPolicyFilePath = "";
+        InputStream is = null;
+
+        final String contextPath = request.getContextPath();
+
+        if(contextPath.equals("/creator")) {
+            try {
+                final URL baseURL =
+                    new URL(moduleBaseURL + strongName + ".gwt.rpc");
+                final URLConnection baseURLConnection =
+                    baseURL.openConnection();
+                is = baseURLConnection.getInputStream();
+            } catch(final Exception ex) {
+                final String message = "ERROR: Could not open policy file "
+                    + ex.toString();
+                getServletContext().log(message);
+                return serializationPolicy;
+            }
+        }
+        try {
+            if (is != null) {
+                try {
+                    serializationPolicy =
+                        SerializationPolicyLoader.loadFromStream(is, null);
+                } catch (final ParseException e) {
+                    getServletContext().log(
+                        "ERROR: Failed to parse the policy file '"
+                        + serializationPolicyFilePath + "'", e);
+                } catch (final IOException e) {
+                    getServletContext().log(
+                        "ERROR: Could not read the policy file '"
+                        + serializationPolicyFilePath + "'", e);
+                }
+            } else {
+                final String message = "ERROR: The serialization policy file '"
+                    + serializationPolicyFilePath
+                    + "' was not found";
+                getServletContext().log(message);
+            }
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (final IOException e) {
+                    // Ignore
+                }
+            }
+        }
+        return serializationPolicy;
+    }
 }
