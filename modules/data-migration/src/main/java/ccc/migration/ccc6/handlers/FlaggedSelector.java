44b07e9dc4d202b6bb54157dea3a942bc8f0f42a
Keith Webster Johnston <k.w.johnston@gmail.com>
2009-04-29 11:58:15 +0000

    #422: Added migration of the NEWS_FLAG field.

---
+++ modules/data-migration/src/main/java/ccc/migration/ccc6/handlers/FlaggedSelector.java
@@ -0,0 +1,37 @@
+package ccc.migration.ccc6.handlers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import ccc.commons.DBC;
+
+/**
+ * TODO: Add Description for this type.
+ *
+ * @author Civic Computing Ltd.
+ */
+public final class FlaggedSelector
+    implements
+        SqlQuery<String> {
+
+    /** {@inheritDoc} */
+    @Override
+    public String handle(final ResultSet rs) throws SQLException {
+        if (rs.next()) {
+            final String result = rs.getString("news_flag");
+            DBC.ensure().toBeFalse(rs.next());
+            return result;
+        }
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getSql() {
+        return
+            "SELECT news_flag "
+          + "FROM c3_pages "
+          + "WHERE page_id=? "
+          + "AND version_id=0";
+    }
+}
