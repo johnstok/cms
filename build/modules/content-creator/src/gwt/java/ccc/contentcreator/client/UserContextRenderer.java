72af6176a728ea9f5ca1091f29d978249ef8abe6
Petteri <petteri@civicuk.com>
2009-05-13 14:56:13 +0000

    Added missing comments.

--- build/modules/content-creator/src/gwt/java/ccc/contentcreator/client/UserContextRenderer.java
+++ build/modules/content-creator/src/gwt/java/ccc/contentcreator/client/UserContextRenderer.java
@@ -1,3 +1,14 @@
+/*-----------------------------------------------------------------------------
+ * Copyright (c) 2009 Civic Computing Ltd.
+ * All rights reserved.
+ *
+ * Revision      $Rev$
+ * Modified by   $Author$
+ * Modified on   $Date$
+ *
+ * Changes: see subversion log.
+ *-----------------------------------------------------------------------------
+ */
 package ccc.contentcreator.client;
 
 import ccc.contentcreator.binding.UserSummaryModelData;
@@ -9,6 +20,11 @@ import com.extjs.gxt.ui.client.widget.grid.ColumnData;
 import com.extjs.gxt.ui.client.widget.grid.GridCellRenderer;
 
 
+/**
+ * TODO: Add Description for this type.
+ *
+ * @author Civic Computing Ltd.
+ */
 public class UserContextRenderer implements GridCellRenderer<ModelData> {
 
     @Override
