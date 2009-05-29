69f7995d64e17d18b166c2fa37bc2809c250afbd
Keith Webster Johnston <k.w.johnston@gmail.com>
2009-05-29 13:42:35 +0000

    Fixed a bug clearing working copies.

--- build/modules/core-domain/src/main/java/ccc/commands/ClearWorkingCopyCommand.java
+++ build/modules/core-domain/src/main/java/ccc/commands/ClearWorkingCopyCommand.java
@@ -72,7 +72,7 @@ public class ClearWorkingCopyCommand {
                 happenedOn,
                 resourceId,
                 null,
-                null,
+                "{}",
                 false));
     }
 }
