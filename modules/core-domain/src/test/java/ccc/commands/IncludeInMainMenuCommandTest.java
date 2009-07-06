fe74f06fb0367cf56105cdae98a814e133383229
Keith Webster Johnston <k.w.johnston@gmail.com>
2009-07-06 16:43:36 +0000

    Added a test for the 'include in main menu' command.

---
+++ modules/core-domain/src/test/java/ccc/commands/IncludeInMainMenuCommandTest.java
@@ -0,0 +1,82 @@
+/*-----------------------------------------------------------------------------
+ * Copyright (c) 2009 Civic Computing Ltd.
+ * All rights reserved.
+ *
+ * Revision      $Rev$
+ * Modified by   $Author$
+ * Modified on   $Date$
+ *
+ * Changes: see subversion log.
+ *-----------------------------------------------------------------------------
+ */
+package ccc.commands;
+
+import static org.easymock.EasyMock.*;
+import ccc.domain.LogEntry;
+import ccc.domain.RemoteExceptionSupport;
+import ccc.domain.Resource;
+import ccc.domain.Search;
+
+
+
+/**
+ * Tests for the {@link IncludeInMainMenuCommand} class.
+ *
+ * @author Civic Computing Ltd.
+ */
+public class IncludeInMainMenuCommandTest
+    extends
+        AbstractCommandTest {
+
+    /**
+     * Test.
+     * @throws RemoteExceptionSupport If the test fails.
+     */
+    public void testIncludeInMainMenu() throws RemoteExceptionSupport {
+
+        // ARRANGE
+        final Search s = new Search("foo");
+        s.lock(_user);
+
+        expect(_dao.find(Resource.class, s.id())).andReturn(s);
+        _audit.record(isA(LogEntry.class));
+
+        replayAll();
+
+        final IncludeInMainMenuCommand c =
+            new IncludeInMainMenuCommand(_dao, _audit);
+
+        // ACT
+        c.execute(_user, _now, s.id(), true);
+
+        // ASSERT
+        verifyAll();
+        assertTrue(s.includeInMainMenu());
+    }
+
+    /**
+     * Test.
+     * @throws RemoteExceptionSupport If the test fails.
+     */
+    public void testRemoveFromMainMenu() throws RemoteExceptionSupport {
+
+        // ARRANGE
+        final Search s = new Search("foo");
+        s.lock(_user);
+
+        expect(_dao.find(Resource.class, s.id())).andReturn(s);
+        _audit.record(isA(LogEntry.class));
+
+        replayAll();
+
+        final IncludeInMainMenuCommand c =
+            new IncludeInMainMenuCommand(_dao, _audit);
+
+        // ACT
+        c.execute(_user, _now, s.id(), false);
+
+        // ASSERT
+        verifyAll();
+        assertFalse(s.includeInMainMenu());
+    }
+}
