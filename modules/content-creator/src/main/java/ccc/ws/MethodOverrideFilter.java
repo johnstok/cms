2b83b58b49eee92da67df1e3f8b1fefdd3bf3b79
Keith Webster Johnston <k.w.johnston@gmail.com>
2009-07-17 12:11:16 +0000

    #511: Added classes to support Google's 'X-HTTP-Method-Override' header.

---
+++ modules/content-creator/src/main/java/ccc/ws/MethodOverrideFilter.java
@@ -0,0 +1,57 @@
+/*-----------------------------------------------------------------------------
+ * Copyright (c) 2009 Civic Computing Ltd.
+ * All rights reserved.
+ *
+ * Revision      $Rev$
+ * Modified by   $Author$
+ * Modified on   $Date$
+ *
+ * Changes: see subversion log.
+ *-----------------------------------------------------------------------------
+ */
+package ccc.ws;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * TODO: Add a description for this type.
+ *
+ * @author Civic Computing Ltd.
+ */
+public class MethodOverrideFilter
+    implements
+        Filter {
+
+
+    /** {@inheritDoc} */
+    @Override public void destroy() { /* No-op */ }
+
+
+    /** {@inheritDoc} */
+    @Override
+    public void doFilter(final ServletRequest request,
+                         final ServletResponse response,
+                         final FilterChain chain) throws IOException,
+                                                         ServletException {
+        if (request instanceof HttpServletRequest) {
+            chain.doFilter(
+                new XHttpServletRequest((HttpServletRequest) request),
+                response);
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+
+    /** {@inheritDoc} */
+    @Override public void init(final FilterConfig filterConfig) { /* No-op */ }
+}
