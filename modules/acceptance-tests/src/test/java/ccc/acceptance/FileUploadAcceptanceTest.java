f023bb10813320582e3794fd624d5a47f67e35c9
Keith Webster Johnston <k.w.johnston@gmail.com>
2009-08-14 11:09:41 +0000

    #551: Added tests for file working copies.

--- modules/acceptance-tests/src/test/java/ccc/acceptance/FileUploadAcceptanceTest.java
+++ modules/acceptance-tests/src/test/java/ccc/acceptance/FileUploadAcceptanceTest.java
@@ -12,10 +12,14 @@
 package ccc.acceptance;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.UUID;
 
 import ccc.api.FailureCode;
+import ccc.api.LogEntrySummary;
 import ccc.api.ResourceSummary;
+import ccc.api.rest.ResourceRevisionPU;
 import ccc.commands.CommandFailedException;
 
 
@@ -27,6 +31,81 @@ import ccc.commands.CommandFailedException;
 public class FileUploadAcceptanceTest
     extends
         AbstractAcceptanceTest {
+
+    /**
+     * Test.
+     *
+     * @throws Exception If the test fails.
+     */
+    public void testCancelWorkingCopy() throws Exception {
+
+        // ARRANGE
+        final String fName = UUID.randomUUID().toString();
+        final ResourceSummary content = _queries.resourceForPath("/content");
+        final ResourceSummary file = createFile(fName, "Hello!", content);
+
+        _commands.lock(file.getId());
+        _commands.createWorkingCopy(file.getId(), new ResourceRevisionPU(0));
+
+        // ACT
+        _commands.clearWorkingCopy(file.getId());
+
+        // ASSERT
+        final ResourceSummary fWC = _queries.resource(file.getId());
+        assertEquals("Hello!", previewContent(file, false));
+        assertFalse(fWC.isHasWorkingCopy());
+    }
+
+    /**
+     * Test.
+     *
+     * @throws Exception If the test fails.
+     */
+    public void testWorkingCopySupport() throws Exception {
+
+        // ARRANGE
+        final String fName = UUID.randomUUID().toString();
+        final ResourceSummary content = _queries.resourceForPath("/content");
+        final ResourceSummary file = createFile(fName, "Hello!", content);
+
+        // History for first revision
+        Collection<LogEntrySummary> revs = _queries.history(file.getId());
+        assertEquals(1, revs.size());
+        final LogEntrySummary rev1 = revs.iterator().next();
+        assertEquals(0, rev1.getIndex());
+        assertEquals("Hello!", previewContent(file, false));
+
+        // Add a second revision
+        _commands.lock(file.getId());
+        updateTextFile("Update!", file);
+        revs = _queries.history(file.getId());
+        assertEquals(2, revs.size());
+        Iterator<LogEntrySummary> i = revs.iterator();
+        i.next();
+        final LogEntrySummary rev2 = i.next();
+        assertEquals(1, rev2.getIndex());
+        assertEquals("Update!", previewContent(file, false));
+
+        // Create working copy from rev 0.
+        _commands.createWorkingCopy(file.getId(), new ResourceRevisionPU(0));
+        ResourceSummary fWC = _queries.resource(file.getId());
+        assertEquals("Update!", previewContent(file, false));
+        assertEquals("Hello!", previewContent(file, true));
+        assertTrue(fWC.isHasWorkingCopy());
+
+        // Apply working copy
+        _commands.applyWorkingCopy(file.getId());
+        revs = _queries.history(file.getId());
+        assertEquals(3, revs.size());
+        i = revs.iterator();
+        i.next();
+        i.next();
+        final LogEntrySummary rev3 = i.next();
+        assertEquals(2, rev3.getIndex());
+        assertEquals("Hello!", previewContent(file, false));
+        fWC = _queries.resource(file.getId());
+        assertFalse(fWC.isHasWorkingCopy());
+    }
 
     /**
      * Test.
@@ -48,7 +127,7 @@ public class FileUploadAcceptanceTest
         // ASSERT
         assertEquals(fName, rs.getName());
         assertEquals("/content/files/"+fName, rs.getAbsolutePath());
-        assertEquals("Hello!", previewContent(rs));
+        assertEquals("Hello!", previewContent(rs, false));
     }
 
 
@@ -79,7 +158,7 @@ public class FileUploadAcceptanceTest
         // ASSERT
         assertEquals(fName, rs.getName());
         assertEquals("/content/files/"+fName, rs.getAbsolutePath());
-        assertEquals("Hello!", previewContent(rs));
+        assertEquals("Hello!", previewContent(rs, false));
     }
 
 
@@ -104,7 +183,7 @@ public class FileUploadAcceptanceTest
 
         // ASSERT
         assertEquals("NULL", body);
-        assertEquals("Update!", previewContent(rs));
+        assertEquals("Update!", previewContent(rs, false));
     }
 
 
@@ -132,6 +211,6 @@ public class FileUploadAcceptanceTest
         } catch (final CommandFailedException e) {
             assertEquals(FailureCode.UNLOCKED, e.getCode());
         }
-        assertEquals("Hello!", previewContent(rs));
+        assertEquals("Hello!", previewContent(rs, false));
     }
 }
