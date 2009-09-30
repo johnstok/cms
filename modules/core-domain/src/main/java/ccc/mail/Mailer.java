702f7923e9076340267e9357a413d65935d5bfbe
Keith Webster Johnston <k.w.johnston@gmail.com>
2009-09-30 11:53:18 +0000

    #656: Added declarative configuration of JavaMail sessions.

--- modules/core-domain/src/main/java/ccc/mail/Mailer.java
+++ modules/core-domain/src/main/java/ccc/mail/Mailer.java
@@ -12,12 +12,10 @@
 package ccc.mail;
 
 import java.util.Date;
-import java.util.Properties;
 
 import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
@@ -26,7 +24,8 @@ import javax.mail.internet.MimeMessage;
 
 import org.apache.log4j.Logger;
 
-import ccc.domain.User;
+import ccc.commons.JNDI;
+import ccc.commons.Registry;
 
 
 /**
@@ -39,73 +38,35 @@ public class Mailer
         Authenticator {
     private static final Logger LOG = Logger.getLogger(Mailer.class);
 
-    private final String _username;
-    private final String _password;
-    private final String _fromAddress;
-    private final String _smtpHost;
-
-
-    /**
-     * Constructor.
-     *
-     * @param username The username for the SMTP server.
-     * @param password The password for the SMTP server.
-     * @param smtpHost The host name for the SMTP server.
-     * @param fromAddress The 'from' address for email messages.
-     */
-    public Mailer(final String username,
-                  final String password,
-                  final String fromAddress,
-                  final String smtpHost) {
-        _username = username;
-        _password = password;
-        _fromAddress = fromAddress;
-        _smtpHost = smtpHost;
-    }
-
-
-    /** {@inheritDoc} */
-    @Override public PasswordAuthentication getPasswordAuthentication() {
-        return new PasswordAuthentication(_username, _password);
-    }
 
     /**
      * Sends a plain text message to the specified recipient.
      *
-     * @param recipient The user who will receive the email.
+     * @param toAddress The address to which the email will be sent.
      * @param subject The email's subject.
      * @param message The email's body.
      *
      * @return True if the mail was sent successfully, false otherwise.
      */
-    public boolean send(final User recipient,
+    public boolean send(final String toAddress,
                         final String subject,
                         final String message) {
         try {
-            final boolean debug = false;
-
-            // Transport properties.
-            final Properties props = new Properties();
-            props.put("mail.smtp.host", _smtpHost);
-            props.put("mail.smtp.auth", "true");
 
             // Create the session.
-            final Session session =
-                Session.getDefaultInstance(props, this);
-            session.setDebug(debug);
+            final Registry r = new JNDI();
+            final Session session = r.get("java:/mail/ash");
 
             // Create the message.
             final Message msg = new MimeMessage(session);
 
             // Set the to / from addresses.
             msg.setRecipient(
-                Message.RecipientType.TO,
-                new InternetAddress(recipient.email().toString()));
-            msg.setFrom(
-                new InternetAddress(_fromAddress));
+                Message.RecipientType.TO, new InternetAddress(toAddress));
 
             // Set the content.
             msg.setSentDate(new Date());
+            msg.setFrom();
             msg.setSubject(subject);
             msg.setContent(message, "text/plain");
 
