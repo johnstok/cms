4d6cda8f15ed64de8f8acb8ee3ffe99a7bf05ecd
Petteri <petteri@civicuk.com>
2009-10-01 08:23:04 +0000

    #584: Fixed trigger fields to use right method to prevent editing.

--- build/modules/client-gwt/src/main/java/ccc/contentcreator/dialogs/CreateAliasDialog.java
+++ build/modules/client-gwt/src/main/java/ccc/contentcreator/dialogs/CreateAliasDialog.java
@@ -73,7 +73,7 @@ public class CreateAliasDialog extends AbstractEditDialog {
         _parentFolder.setFieldLabel(constants().folder());
         _parentFolder.setId("parent-folder");
         _parentFolder.setValue("");
-        _parentFolder.setReadOnly(true);
+        _parentFolder.setEditable(false);
         _parentFolder.addListener(
             Events.TriggerClick,
             new Listener<ComponentEvent>(){
