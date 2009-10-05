8f55ea82a1e00df5a9b2a8ebba859dfe39745188
Keith Webster Johnston <k.w.johnston@gmail.com>
2009-10-05 10:35:27 +0000

    #666: Removed offending methods. All querying for resources now happens through the ResourceRepository interface.

--- modules/core-domain/src/main/java/ccc/persistence/FileRepositoryImpl.java
+++ modules/core-domain/src/main/java/ccc/persistence/FileRepositoryImpl.java
@@ -14,13 +14,11 @@ package ccc.persistence;
 
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 
 import ccc.domain.Data;
 import ccc.domain.EntityNotFoundException;
-import ccc.domain.File;
 import ccc.domain.Setting;
 import ccc.entities.IData;
 import ccc.persistence.streams.CopyAction;
@@ -35,32 +33,18 @@ import ccc.types.DBC;
  */
 public class FileRepositoryImpl implements FileRepository {
 
-    private Repository _repository;
     private CoreData _cd;
 
     /**
      * Constructor.
      *
      * @param cd The JDBC datasource used to manage data.
-     * @param repository The DAO used for CRUD operations, etc.
      */
-    public FileRepositoryImpl(final CoreData cd, final Repository repository) {
+    public FileRepositoryImpl(final CoreData cd) {
         DBC.require().notNull(cd);
-        DBC.require().notNull(repository);
         _cd = cd;
-        _repository = repository;
     }
 
-
-    /**
-     * Constructor.
-     *
-     * @param cd The JDBC datasource used to manage data.
-     * @param em The JPA entity manager for this repository.
-     */
-    public FileRepositoryImpl(final CoreData cd, final EntityManager em) {
-        this(cd, new JpaRepository(em));
-    }
 
     /**
      * Create a file repository that reads / writes to the file system.
@@ -74,20 +58,13 @@ public class FileRepositoryImpl implements FileRepository {
             final SettingsRepository settings = new SettingsRepository(em);
             final Setting filestorePath =
                 settings.find(Setting.Name.FILE_STORE_PATH);
-            return new FileRepositoryImpl(
-                new FsCoreData(filestorePath.value()),
-                em);
+            return
+                new FileRepositoryImpl(new FsCoreData(filestorePath.value()));
+
         } catch (final EntityNotFoundException e) {
             throw new RuntimeException(
                 "Setting missing: "+Setting.Name.FILE_STORE_PATH);
         }
-    }
-
-
-    /** {@inheritDoc} */
-    @Override
-    public List<File> findImages() {
-        return _repository.list(QueryNames.ALL_IMAGES, File.class);
     }
 
 
