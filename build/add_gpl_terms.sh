debf8af0b3b4383653ce8f08a25f1e24b195ff95
Keith Webster Johnston <k.w.johnston@gmail.com>
2009-12-07 17:38:25 +0000

    #586: Updated sed script with correct text.

--- build/add_gpl_terms.sh
+++ build/add_gpl_terms.sh
@@ -17,19 +17,19 @@ for f in $(find -name '*.java'); do
     sed -i '
     /^ \* All rights reserved.$/ a\
  *\
- * This file is part of CCC.\
+ * This file is part of Content Control.\
  *\
- * CCC is free software: you can redistribute it and/or modify\
+ * Content Control is free software: you can redistribute it and/or modify\
  * it under the terms of the GNU General Public License as published by\
  * the Free Software Foundation, either version 3 of the License, or\
  * (at your option) any later version.\
  *\
- * CCC is distributed in the hope that it will be useful,\
+ * Content Control is distributed in the hope that it will be useful,\
  * but WITHOUT ANY WARRANTY; without even the implied warranty of\
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\
  * GNU General Public License for more details.\
  *\
  * You should have received a copy of the GNU General Public License\
- * along with CCC.  If not, see <http://www.gnu.org/licenses/>.' $f
+ * along with Content Control.  If not, see http://www.gnu.org/licenses/.' $f
 done
 
