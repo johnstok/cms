48535071d9cc40d06909fe89ee1ad704af3822f3
Keith Webster Johnston <k.w.johnston@gmail.com>
2009-12-07 17:46:54 +0000

    #586: Added GPL copying permission statement to all Java files.

--- modules/services-ejb3/src/main/java/ccc/services/ejb3/ActionsSchedulerEJB.java
+++ modules/services-ejb3/src/main/java/ccc/services/ejb3/ActionsSchedulerEJB.java
@@ -2,6 +2,21 @@
  * Copyright (c) 2009 Civic Computing Ltd.
  * All rights reserved.
  *
+ * This file is part of Content Control.
+ *
+ * Content Control is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Content Control is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Content Control.  If not, see http://www.gnu.org/licenses/.
+ *
  * Revision      $Rev: 1946 $
  * Modified by   $Author: keith $
  * Modified on   $Date: 2009-10-05 17:07:14 +0100 (Mon, 05 Oct 2009) $
