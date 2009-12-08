77dbfd4df77f318750ad870087a79aea0acd1ca9
Keith Webster Johnston <k.w.johnston@gmail.com>
2009-12-08 11:13:33 +0000

    #715: Updated to change the relevant files in-place.

--- modules/application-ear/rename.sh
+++ modules/application-ear/rename.sh
@@ -1,38 +1,27 @@
 # TODO check arguments
-# TODO other datasources
-echo "Renaming CCC configuration files."
+# Limit changes to old values prefixed by the following chars >/=":
 
-echo
-echo "Fixing application.xml:"
-sed -n 's/'"$1"'/'"$2"'/gp' target/application-ear-7.0.0-SNAPSHOT/META-INF/application.xml
+function replace_name {
+    echo
+    echo Fixing $3:
+    sed -r -n 's/([^a-zA-Z]+)'"$1"'/\1'"$2"'/gp' $3
+    sed -r -i 's/([^a-zA-Z]+)'"$1"'/\1'"$2"'/g' $3
+}
 
-echo
-echo "Fixing jboss-app.xml:"
-sed -n 's/'"$1"'/'"$2"'/gp' target/application-ear-7.0.0-SNAPSHOT/META-INF/jboss-app.xml
+echo Renaming CCC configuration files.
+echo Replacing $1 with $2
 
-echo
-echo "Fixing persistence.xml:"
-sed -n 's/'"$1"'/'"$2"'/gp' target/application-ear-7.0.0-SNAPSHOT/META-INF/persistence.xml
-
-echo
-echo "Fixing auth-config.xml:"
-sed -n 's/'"$1"'/'"$2"'/gp' target/application-ear-7.0.0-SNAPSHOT/auth-config.xml
-
-echo
-echo "Fixing auth-service.xml:"
-sed -n 's/'"$1"'/'"$2"'/gp' target/application-ear-7.0.0-SNAPSHOT/auth-service.xml
-
-echo
-echo "Fixing mail-service.xml:"
-sed -n 's/'"$1"'/'"$2"'/gp' target/application-ear-7.0.0-SNAPSHOT/mail-service.xml
-
-echo
-echo "Fixing oracle-ds.xml:"
-sed -n 's/'"$1"'/'"$2"'/gp' target/application-ear-7.0.0-SNAPSHOT/oracle-ds.xml
-
-echo
-echo "Fixing web.xml:"
-sed -n 's/'"$1"'/'"$2"'/gp' target/application-ear-7.0.0-SNAPSHOT/content-creator-7.0.0-SNAPSHOT.war/WEB-INF/web.xml
+replace_name $1 $2 META-INF/application.xml
+replace_name $1 $2 META-INF/jboss-app.xml
+replace_name $1 $2 services-ejb3-7.0.0-SNAPSHOT.jar/META-INF/persistence.xml
+replace_name $1 $2 auth-config.xml
+replace_name $1 $2 auth-service.xml
+replace_name $1 $2 mail-service.xml
+replace_name $1 $2 oracle-ds.xml
+replace_name $1 $2 mysql-ds.xml
+replace_name $1 $2 h2-ds.xml
+replace_name $1 $2 mssql-ds.xml
+replace_name $1 $2 content-creator-7.0.0-SNAPSHOT.war/WEB-INF/web.xml
 
 echo
 
