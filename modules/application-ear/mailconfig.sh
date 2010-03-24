a6ab0581fa3f790256fff4ffbcb06d5e1af93166
Keith Webster Johnston <k.w.johnston@gmail.com>
2010-03-24 15:58:47 +0000

    Fixed incorrect path in mail config script.

--- modules/application-ear/mailconfig.sh
+++ modules/application-ear/mailconfig.sh
@@ -35,10 +35,10 @@ then
 exit
 fi
 
-$SED -i 's/<attribute name=\"User\">[^<]*<\/attribute>/<attribute name=\"User\">'"$mail_username"'<\/attribute>/' "mail-service (copy).xml"
-$SED -i 's/<attribute name=\"Password\">[^<]*<\/attribute>/<attribute name=\"Password\">'"$mail_password"'<\/attribute>/' "mail-service (copy).xml"
-$SED -i 's/<property name=\"mail.user\" value=\"[^\"]*\"/<property name=\"mail.user\" value=\"'"$mail_username"'\"/' "mail-service (copy).xml"
-$SED -i 's/<property name=\"mail.host\" value=\"[^\"]*\"/<property name=\"mail.host\" value=\"'"$mail_host"'\"/' "mail-service (copy).xml"
-$SED -i 's/<property name=\"mail.pop3.host\" value=\"mail\.[^\"]*\"/<property name=\"mail.pop3.host\" value=\"mail.'"$mail_host"'\"/' "mail-service (copy).xml"
-$SED -i 's/<property name=\"mail.smtp.host\" value=\"mail\.[^\"]*\"/<property name=\"mail.smtp.host\" value=\"mail.'"$mail_host"'\"/' "mail-service (copy).xml"
+$SED -i 's/<attribute name=\"User\">[^<]*<\/attribute>/<attribute name=\"User\">'"$mail_username"'<\/attribute>/' mail-service.xml
+$SED -i 's/<attribute name=\"Password\">[^<]*<\/attribute>/<attribute name=\"Password\">'"$mail_password"'<\/attribute>/' mail-service.xml
+$SED -i 's/<property name=\"mail.user\" value=\"[^\"]*\"/<property name=\"mail.user\" value=\"'"$mail_username"'\"/' mail-service.xml
+$SED -i 's/<property name=\"mail.host\" value=\"[^\"]*\"/<property name=\"mail.host\" value=\"'"$mail_host"'\"/' mail-service.xml
+$SED -i 's/<property name=\"mail.pop3.host\" value=\"mail\.[^\"]*\"/<property name=\"mail.pop3.host\" value=\"mail.'"$mail_host"'\"/' mail-service.xml
+$SED -i 's/<property name=\"mail.smtp.host\" value=\"mail\.[^\"]*\"/<property name=\"mail.smtp.host\" value=\"mail.'"$mail_host"'\"/' mail-service.xml
 
