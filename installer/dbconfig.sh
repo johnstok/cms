f223486a07e40a4f94d7b623e5294b6761c0a500
Keith Webster Johnston <k.w.johnston@gmail.com>
2010-07-02 15:28:25 +0000

    CC-1092: Enabled XA for all supported databases.

--- installer/dbconfig.sh
+++ installer/dbconfig.sh
@@ -14,19 +14,19 @@ DB_PASSWORD=$4
 case "$dbtype" in
         1)
                 dialect="org.hibernate.dialect.H2Dialect";
-                driver="org.h2.jdbcx.JdbcDataSource";
+                datasource="org.h2.jdbcx.JdbcDataSource";
                 ;;
         2)
                 dialect="org.hibernate.dialect.Oracle9Dialect";
-                driver="oracle.jdbc.driver.OracleDriver";
+                datasource="oracle.jdbc.xa.client.OracleXADataSource";
                 ;;
         3)
                 dialect="org.hibernate.dialect.MySQL5InnoDBDialect";
-                driver="com.mysql.jdbc.Driver";
+                datasource="com.mysql.jdbc.jdbc2.optional.MysqlXADataSource";
                 ;;
         4)
                 dialect="org.hibernate.dialect.SQLServerDialect";
-                driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+                datasource="com.microsoft.sqlserver.jdbc.SQLServerXADataSource";
                 ;;
         *)
                 echo WARNING Invalid database type selected - ignoring;
@@ -35,7 +35,7 @@ esac
 
 $SED -i 's/<property name=\"hibernate.dialect\" value=\"[^\"]*\"/<property name=\"hibernate.dialect\" value=\"'"$dialect"'\"/' services-ejb3.jar/META-INF/persistence.xml
 
-$SED -i 's#<connection-url>[^<]*</connection-url>#<connection-url>'"$DB_URL"'</connection-url>#'g database-ds.xml
-$SED -i 's/<driver-class>[^<]*<\/driver-class>/<driver-class>'"$driver"'<\/driver-class>/' database-ds.xml
+$SED -i 's/<xa-datasource-property name=\"URL\">[^<]*<\/xa-datasource-property>/<xa-datasource-property name=\"URL\">'"$DB_URL"'<\/xa-datasource-property>/' database-ds.xml
+$SED -i 's/<xa-datasource-class>[^<]*<\/xa-datasource-class>/<xa-datasource-class>'"$datasource"'<\/xa-datasource-class>/' database-ds.xml
 $SED -i 's/<user-name>[^<]*<\/user-name>/<user-name>'"$DB_USERNAME"'<\/user-name>/' database-ds.xml
 $SED -i 's/<password>[^<]*<\/password>/<password>'"$DB_PASSWORD"'<\/password>/' database-ds.xml
