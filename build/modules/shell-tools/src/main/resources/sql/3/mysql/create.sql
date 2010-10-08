3c9b2dfabf09959af4dcb12aba39efbe0bf8d848
Petteri <petteri@civicuk.com>
2010-10-08 12:59:35 +0000

    CC-881: Changed constrain name to lower case.

--- build/modules/shell-tools/src/main/resources/sql/3/mysql/create.sql
+++ build/modules/shell-tools/src/main/resources/sql/3/mysql/create.sql
@@ -4,4 +4,4 @@ ALTER TABLE page_revision_paragraphs ADD index_text varchar(1024);
 UPDATE page_revision_paragraphs SET index_text=substring(value_text, 1, 1024) WHERE type='TEXT';
 
 ALTER TABLE folders DROP FOREIGN KEY FK_FOLDER_INDEX_PAGE_ID;
-ALTER TABLE folders ADD CONSTRAINT FK_FOLDER_INDEX_PAGE_ID FOREIGN KEY (index_page) REFERENCES resources (id);
+ALTER TABLE folders ADD CONSTRAINT fk_folder_index_page_id foreign key (index_page) REFERENCES resources (id);
