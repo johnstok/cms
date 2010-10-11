2ffd6c1a221a600fbded926b980d88299659b7a8
Keith Webster Johnston <k.w.johnston@gmail.com>
2010-10-11 15:34:51 +0000

    CC-1144: Added hierarchical views for Oracle DB.

--- build/modules/shell-tools/src/main/resources/sql/3/oracle/create.sql
+++ build/modules/shell-tools/src/main/resources/sql/3/oracle/create.sql
@@ -5,3 +5,13 @@ UPDATE page_revision_paragraphs SET index_text=dbms_lob.substr(value_text, 1024)
 
 ALTER TABLE folders DROP CONSTRAINT fk_folder_index_page_id;
 ALTER TABLE folders ADD  CONSTRAINT fk_folder_index_page_id FOREIGN KEY (index_page) REFERENCES resources;
+
+CREATE OR REPLACE VIEW resource_metadata_by_relevance AS SELECT r.id resource_id, m.datum_key, m.datum_value, instr( r.id_path, m.resource_id) relevance FROM (SELECT id, SYS_CONNECT_BY_PATH(id, '/') id_path FROM resources START WITH parent_id is null CONNECT BY prior id = parent_id) r, resource_metadata m WHERE id_path LIKE '%/'||m.resource_id||'%';
+
+CREATE OR REPLACE VIEW resource_metadata_hierarchical AS SELECT meta.* FROM resource_metadata_by_relevance meta, (SELECT resource_id, datum_key, max(relevance) most_relevant FROM resource_metadata_by_relevance GROUP BY resource_id, datum_key) relevant WHERE meta.datum_key=relevant.datum_key AND meta.resource_id=relevant.resource_id AND meta.relevance=relevant.most_relevant;
+
+CREATE OR REPLACE VIEW resource_hierarchical AS SELECT id resource_id, CASE WHEN length(SYS_CONNECT_BY_PATH(published_by_id, '/'))<(37*level) THEN 0 ELSE 1 END published, CASE WHEN to_number(replace(SYS_CONNECT_BY_PATH(perms, '/'), '/', '0')) > 0 THEN 1 ELSE 0 END secure, SYS_CONNECT_BY_PATH(name, '/') absolute_path FROM (SELECT r.*, (SELECT count(*) FROM resource_users u WHERE r.id = u.resource_id) + (SELECT count(*) FROM resource_roles g WHERE r.id = g.resource_id) perms FROM resources r) START WITH parent_id is null CONNECT BY prior id = parent_id;
+
+CREATE OR REPLACE VIEW resource_template_hierarchical AS SELECT id resource_id, substr(template, instr(template, '/', -1)+1) template_id FROM (SELECT id, rtrim(SYS_CONNECT_BY_PATH(template_id, '/'), '/') template FROM resources r START WITH parent_id is null CONNECT BY prior id = parent_id);
+  
+CREATE OR REPLACE VIEW resource_tags_hierarchical AS SELECT DISTINCT r.id resource_id, t.tag_value FROM (SELECT id, SYS_CONNECT_BY_PATH(id, '/') id_path FROM resources START WITH parent_id is null CONNECT BY prior id = parent_id) r, resource_tags t WHERE id_path LIKE '%/'||t.resource_id||'%';
---
+++ build/modules/shell-tools/src/main/resources/sql/3/oracle/drop.sql
@@ -0,0 +1,5 @@
+DROP VIEW resource_metadata_by_relevance;
+DROP VIEW resource_metadata_hierarchical;
+DROP VIEW resource_hierarchical;
+DROP VIEW resource_template_hierarchical;
+DROP VIEW resource_tags_hierarchical;
