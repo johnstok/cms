eee0b049201698838ca5321adefd9cb16dedcf55
Petteri <petteri@civicuk.com>
2010-10-12 08:44:04 +0000

    CC-1130: Fixed another case issue in a constrain drop.

--- build/modules/shell-tools/src/main/resources/sql/3/sqlserver/create.sql
+++ build/modules/shell-tools/src/main/resources/sql/3/sqlserver/create.sql
@@ -4,4 +4,4 @@ ALTER TABLE page_revision_paragraphs ADD index_text nvarchar(1024);
 UPDATE page_revision_paragraphs SET index_text=substring(value_text, 1, 1024) WHERE type='TEXT';
 
 ALTER TABLE folders DROP CONSTRAINT FK_FOLDER_INDEX_PAGE_ID;
-ALTER TABLE folders ADD  CONSTRAINT fk_folder_index_page_id FOREIGN KEY (index_page) REFERENCES resources;
+ALTER TABLE folders ADD  CONSTRAINT FK_FOLDER_INDEX_PAGE_ID FOREIGN KEY (index_page) REFERENCES resources;
